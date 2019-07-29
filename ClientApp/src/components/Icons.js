a34f54c156f37ac7a5a89bf778d2009c26ca3b17
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-07-29 13:44:10 -0700

    add in all champion icons

--- ClientApp/src/components/Icons.js
+++ ClientApp/src/components/Icons.js
@@ -5,26 +5,68 @@ export class Icons extends Component {
     super();
 
     this.state = {
-      iconUrls: []
+      iconUrls: [],
+      loading: true
     }
   }
-
   componentDidMount() {
     let urls = [];
-    fetch('https://ddragon.leagueoflegends.com/cdn/9.14.1/img/champion/Rengar.png').then(results => {
-      urls.push(results.url);
-      this.setState({iconUrls: urls});
-    });
+    let championNames = ['Aatrox', 'Ahri', 'Akali', 'Anivia', 'Ashe', 'AurelionSol', 'Blitzcrank', 'Brand', 'Braum', 'Chogath', 'Darius', 'Draven', 'Elise', 'Evelynn', 'Fiora', 'Gangplank', 'Garen', 'Gnar', 'Graves', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kennen', 'Khazix', 'Kindred', 'Leona', 'Lissandra', 'Lucian', 'Lulu', 'MissFortune', 'Mordekaiser', 'Morgana', 'Nidalee', 'Poppy', 'Pyke', 'RekSai', 'Rengar', 'Sejuani', 'Shen', 'Shyvana', 'Swain', 'Tristana', 'TwistedFate', 'Varus', 'Vayne', 'Volibear', 'Warwick', 'Yasuo', 'Zed'];
+    
+    championNames.map(champ => {
+      fetch('https://ddragon.leagueoflegends.com/cdn/9.14.1/img/champion/'+champ+'.png').then(results => {
+        urls.push(results.url);
+        this.setState({iconUrls: urls});
+      });
+
+    })
     console.log(this.state.iconUrls);
   }
 
   render() {
     return (
       <div>
-        <img src={this.state.iconUrls[0]} alt='Rengar'/>
+        <img src={this.state.iconUrls[7]} alt='Rengar'/>
       </div>
     )
   }
+  // componentDidMount() {
+  //   let urls = [];
+  //   let championName = 'rengar';
+  //   fetch('https://ddragon.leagueoflegends.com/cdn/9.14.1/img/champion/Rengar.png').then(results => {
+  //     urls.push ({key: championName, value: results.url});
+  //     console.log(urls);
+  //   })
+  //   this.setState({iconUrls: urls});
+  //   this.setState({loading: false});
+  //   this.render();
+  //   console.log('done mounting');
+  // }
+  
+  // render() {
+  //   let foundIndex = null;
+  //   console.log(this.state.iconUrls);
+  //   if(this.state.loading === false) {
+  //     this.state.iconUrls.forEach(index => {
+  //       console.log('test')
+  //       if(index.key === 'rengar') {
+  //         foundIndex = index;
+  //         console.log(foundIndex)
+  //       }
+  //       else {
+  //         console.log('image not found')
+  //       }
+  //     })
+  //     return (
+  //       <div>
+  //         {console.log(foundIndex)};
+  //       </div>
+  //   )
+  // }
+  // else {
+  //   return <div>Loading...</div>
+  // }
+  // }
 }
 
 export default Icons
