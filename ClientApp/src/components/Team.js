0594a382792e26b66dd035f53d93aa9fff6de6db
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-07-31 23:52:33 -0700

    add champion images to team.js

--- ClientApp/src/components/Team.js
+++ ClientApp/src/components/Team.js
@@ -1,9 +1,34 @@
+import PropTypes from 'prop-types';
+import './Team.css';
+
 import React from 'react'
 
-export default function TierList() {
+export default function TierList(props) {
+  // team's state
+  // state of image hidden or not
+
+  function addToTeam() {
+    
+  }
+
   return (
     <div>
       <h1>Team Builder</h1>
+      <div className='team'>Player Team
+      </div>
+      <div className='bonus'>Bonuses</div>
+      <div className='champs'>
+        {props.champIcons.map((champ) => 
+          <img onClick={addToTeam()} src={champ} />
+        )}
+      </div>
     </div>
   )
+}
+
+TierList.propTypes = {
+  champs: PropTypes.object,
+  champIcons: PropTypes.array,
+  items: PropTypes.object,
+  itemIcons: PropTypes.array
 }
