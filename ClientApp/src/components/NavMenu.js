02c47e15c54e16924dbfccd8c8030bb2c0bf3be3
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-07-31 20:14:21 -0700

    update NavMenu.js

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,69 +1,51 @@
-/* App.jsx */
-import React, { Component } from 'react'
+import React from 'react'
+import './NavMenu.css';
+import $ from 'jquery';
 
-/* MenuButton.jsx */
-export class NavMenu extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      open: this.props.open? this.props.open:false,
-      color: this.props.color? this.props.color:'black',
-    }
+export default class NavMenu extends React.Component {
+  constructor() {
+    super();
   }
 
-  componentWillReceiveProps(nextProps){
-    if(nextProps.open !== this.state.open){
-      this.setState({open:nextProps.open});
+  componentDidMount() {
+
+      $( this.refs.toggleInput.getDOMNode() ).bootstrapToggle(data-target);
+
     }
-  }
-  
-  handleClick(){
-  this.setState({open:!this.state.open});
-  }
-  
-  render(){
-    const styles = {
-      container: {
-        height: '32px',
-        width: '32px',
-        display:'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        cursor: 'pointer',
-        padding: '4px',
-      },
-      line: {
-        height: '2px',
-        width: '20px',
-        background: this.state.color,
-        transition: 'all 0.2s ease',
-      },
-      lineTop: {
-        transform: this.state.open ? 'rotate(45deg)':'none',
-        transformOrigin: 'top left',
-        marginBottom: '5px',
-      },
-      lineMiddle: {
-        opacity: this.state.open ? 0: 1,
-        transform: this.state.open ? 'translateX(-16px)':'none',
-      },
-      lineBottom: {
-        transform: this.state.open ? 'translateX(-1px) rotate(-45deg)':'none',
-        transformOrigin: 'top left',
-        marginTop: '5px',
-      },       
-    }
-    return(
-      <div style={styles.container} 
-        onClick={this.props.onClick ? this.props.onClick: 
-          ()=> {this.handleClick();}}>
-        <div style={{...styles.line,...styles.lineTop}}/>
-        <div style={{...styles.line,...styles.lineMiddle}}/>
-        <div style={{...styles.line,...styles.lineBottom}}/>
-      </div>
-    )
-  }
+    render() {
+
+      return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  )
 }
-
-export default NavMenu
+}
