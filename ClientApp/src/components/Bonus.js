b8430cac09b9092f277ff97bc6e5b9202d33fd17
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-08-02 15:18:32 -0700

    add checkShowOrigin() and checkShowClass()

--- ClientApp/src/components/Bonus.js
+++ ClientApp/src/components/Bonus.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import originImages from './GetOrigins';
+import classImages from './GetClasses';
 
 class Bonus extends Component {
   constructor(props) {
@@ -9,27 +11,59 @@ class Bonus extends Component {
       showOrigins: [false,false,false,false,false,false,false,false,false, false, false, false, false],
       showClasses: [false,false,false,false,false,false,false,false,false, false, false],
       originBreakpoints: [],
-      classBreakpoints: []
+      classBreakpoints: [],
     }
+    this.updateBonuses = this.updateBonuses.bind(this);
   }
   
   componentWillMount() {
     
   }
   
-  checkShowOrigin(prop) {
-    
+  checkShowOrigin(amount, index, updatedKeys) {
+    let type=updatedKeys[index];
+    if(amount >= 1) {
+      return <li><img src={originImages()[index]} /> {amount} {type}</li>;
+    }
+    else {
+      return;
+    }
   }
   
-  checkShowClasses() {
-    
+  checkShowClass(amount, index, updatedKeys) {
+    let type=updatedKeys[index];
+    if(amount >= 1) {
+      return <li><img src={classImages()[index]} /> {amount} {type}</li>;
+    }
+    else {
+      return;
+    }
+  }
+
+  updateBonuses() {
+    this.props.bonuses();
+    return this.props.bonuses();
   }
   
   render() {
+    let updatedOrigin = this.updateBonuses()[0];
+    const originKeys = Object.keys(updatedOrigin);
+    const originValues = Object.values(updatedOrigin)
+    let updatedClass = this.updateBonuses()[1];
+    const classKeys = Object.keys(updatedClass);
+    const classValues = Object.values(updatedClass)
     return (
       <div>
         <h1> Bonuses </h1>
-        {/* {console.log(this.bonuses.demon)} */}
+        <ul className='origin'>
+          {originValues.map((amount, index) => 
+            this.checkShowOrigin(amount, index, originKeys))},
+        </ul>
+        <h2>Classes</h2>
+        <ul className='class'>
+          {classValues.map((amount, index) => 
+            this.checkShowClass(amount, index, classKeys))},
+        </ul>
       </div>
     );
   }
--- ClientApp/src/components/Team.js
+++ ClientApp/src/components/Team.js
@@ -2,18 +2,16 @@ import PropTypes from 'prop-types';
 import './Team.css';
 import React, { Component } from 'react'
 import Bonus from './Bonus';
-import originImages from './GetOrigins';
-import classImages from './GetClasses';
 
 export default function Team(props) {
 
   function addToTeam(index, origins, classes) {
+    let duplicate = checkIfDuplicate();
     props.addToTeam(index);
-    updateBonuses();
+    updateBonuses(duplicate);
   }
 
   function findImage(champ) {
-    console.log(champ.key);
     let result = null;
     let championNames = ['Aatrox', 'Ahri', 'Akali', 'Anivia', 'Ashe', 'AurelionSol', 'Blitzcrank', 'Brand', 'Braum', 'Chogath', 'Darius', 'Draven', 'Elise', 'Evelynn', 'Fiora', 'Gangplank', 'Garen', 'Gnar', 'Graves', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kennen', 'Khazix', 'Kindred', 'Leona', 'Lissandra', 'Lucian', 'Lulu', 'MissFortune', 'Mordekaiser', 'Morgana', 'Nidalee', 'Poppy', 'Pyke', 'RekSai', 'Rengar', 'Sejuani', 'Shen', 'Shyvana', 'Swain', 'Tristana', 'TwistedFate', 'Varus', 'Vayne', 'Volibear', 'Warwick', 'Yasuo', 'Zed'];
     championNames.map((name, index) => {
@@ -24,8 +22,11 @@ export default function Team(props) {
     return result;
   }
 
-  function updateBonuses() {
-    let test = "test"
+  checkIfDuplicate() {
+
+  }
+  
+  function updateBonuses(duplicate) {
     let origins = {
       Demon : 0,
       Dragon: 0,
@@ -53,23 +54,21 @@ export default function Team(props) {
       Shapeshifter: 0,
       Sorcerer: 0
     };
-    // order origins - demon, dragon, exile, glacial, imperial, ninja, noble, phantom, pirate, robot, void, wild, yordle
-    // order classes - assassin, blademaster, brawler, elementalist, guardian, gunslinger, blademaster, knight, ranger, shapeshifter, sorcerer
-    console.log(props.userTeam);
+    // fix duplicates issue
     props.userTeam.map((champ) => {
-      console.log(champ)
       champ.origin.map((type) => {
-        console.log(origins);
-        console.log(origins[type]);
-        origins[type] += 1;
+        console.log(((props.userTeam.includes(champ))))
+        if(!(duplicate)) {
+          origins[type] += 1;
+        }
       })
       champ.class.map((type) => {
-        console.log(type);
-        origins[type] += 1;
+        if(!(duplicate)) {
+          classes[type] += 1;
+        }
       })
     })
     let bonuses = [origins, classes];
-    console.log(bonuses);
     return bonuses;
   }
 
@@ -81,41 +80,14 @@ export default function Team(props) {
           <img src={props.champIcons[findImage(champ)]} />
         )}
       </div>
-      {/* <div className='bonus'><Bonus bonuses={origins}/> */}
-      {/* </div> */}
+      <div className='bonus'><Bonus bonuses={updateBonuses}/>
+      </div>
       <div className='champs'>
         {props.champIcons.map((champ, index) => 
           <img onClick={() => addToTeam(index)}  src={champ} />
           )}
       </div>
       <h2>Origins</h2>
-<ul>
-  <li><img src={originImages()[0]} /> {updateBonuses()[0].Demon}</li>
-  <li><img src={originImages()[1]} /> {updateBonuses()[0].Dragon}</li>
-  <li><img src={originImages()[2]} /> {updateBonuses()[0].Exile}</li>
-  <li><img src={originImages()[3]} />{updateBonuses()[0].Glacial}</li>
-  <li><img src={originImages()[4]} />{updateBonuses()[0].Imperial}</li>
-  <li><img src={originImages()[5]} />{updateBonuses()[0].Ninja}</li>
-  <li><img src={originImages()[6]} />{updateBonuses()[0].Noble}</li>
-  <li><img src={originImages()[7]} />{updateBonuses()[0].Phantom}</li>
-  <li><img src={originImages()[8]} />{updateBonuses()[0].Pirate}</li>
-  <li><img src={originImages()[9]} />{updateBonuses()[0].Robot}</li>
-  <li><img src={originImages()[10]} />{updateBonuses()[0].Void}</li>
-  <li><img src={originImages()[11]} />{updateBonuses()[0].Wild}</li>
-  <li><img src={originImages()[12]} />{updateBonuses()[0].Yordle}</li>
-</ul>
-<h2>Classes</h2>
-<ul>
-  <li><img src={classImages()[0]} />{updateBonuses()[1].Assassin}</li>
-  <li><img src={classImages()[1]} />{updateBonuses()[1].Blademaster}</li>
-  <li><img src={classImages()[2]} />{updateBonuses()[1].Brawler}</li>
-  <li><img src={classImages()[3]} />{updateBonuses()[1].Guardian}</li>
-  <li><img src={classImages()[4]} />{updateBonuses()[1].Gunslinger}</li>
-  <li><img src={classImages()[5]} />{updateBonuses()[1].Knight}</li>
-  <li><img src={classImages()[6]} />{updateBonuses()[1].Ranger}</li>
-  <li><img src={classImages()[7]} />{updateBonuses()[1].Shapeshifter}</li>
-  <li><img src={classImages()[8]} />{updateBonuses()[1].Sorcerer}</li>
-</ul>
     </div>
   )
 }
